c18ab344303ee0e6dc09dd198bd074a34a39494c
Ubuntu <ubuntu@ip-172-31-30-176.ap-southeast-1.compute.internal>
2016-12-27 03:38:28 +0000

    add migration, add ignorefile

--- database/migrations/2016_11_23_223418_ServiceProvider.php
+++ database/migrations/2016_11_23_223418_ServiceProvider.php
@@ -17,6 +17,7 @@ class ServiceProvider extends Migration
 		$table->string('name');
 		$table->string('email');
 		$table->string('password');
+		$table->rememberToken();
 		$table->timestamps();
 	});
     }
